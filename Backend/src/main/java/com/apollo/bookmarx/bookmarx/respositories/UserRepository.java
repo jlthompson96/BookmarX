b68e6a14a939b02c528a103a11cb074d879afad9
Joey Thompson <joeythompson1014@gmail.com>
2023-08-27 16:12:15 -0400

    Delete Backend directory

--- Backend/src/main/java/com/apollo/bookmarx/bookmarx/respositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.apollo.bookmarx.bookmarx.respositories;
-
-import com.apollo.bookmarx.bookmarx.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
