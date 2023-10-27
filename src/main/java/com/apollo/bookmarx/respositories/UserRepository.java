8094f2ebcc0dfd2c7ff5010153586841e1779670
Joey Thompson <joeythompson1014@gmail.com>
2023-10-27 10:40:40 -0400

    reverted to JDK17

--- src/main/java/com/apollo/bookmarx/respositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.apollo.bookmarx.respositories;
-
-import com.apollo.bookmarx.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
